fcef6ec2ab080a9d380571fe9af8bb56cbec3b39
XiaochenCui <jcnlcxc.new@gmail.com>
2021-03-07 20:10:05 +0800

    clear

--- src/table/table.rs
+++
@@ -1,8 +0,0 @@
-use crate::{page::Page, row::RowScheme};
-use std::{io, sync::Arc};
-
-pub trait Table<'a> {
-    fn get_id(&self) -> i32;
-    fn get_row_scheme(&self) -> Arc<RowScheme>;
-    fn read_page(&'a mut self, page_id: i32) -> Result<Arc<dyn Page>, io::Error>;
-}
